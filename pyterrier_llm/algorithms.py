7bf1c5cddc4b6770a076a83faf30e59b5ba481d3
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-04-04 15:12:03 +0100

    remove pivot for now

--- pyterrier_llm/algorithms.py
+++ pyterrier_llm/algorithms.py
@@ -1,102 +1,11 @@
 import pandas as pd
-from ._util import RankedList, split, iter_windows
+from ._util import RankedList, iter_windows
 import logging 
 import numpy as np
 from numpy import concatenate as concat
 from enum import Enum
 
 logger = logging.getLogger(__name__)
-
-def _inner_pivot(model, qid : str, query : str, ranking : RankedList):
-        '''
-        l : current left partition being scored
-        r : current right partition being the remainder of the array
-        c : current candidates
-        b : current backfill
-        p : current pivot
-        '''
-        if model.verbose: logger.info(f"Processing query {qid} with {len(ranking)} documents")
-        l = ranking[:model.window_size]
-        r = ranking[model.window_size:]
-
-        kwargs = {
-            'qid': qid,
-            'query': query,
-            'doc_text': l.doc_texts,
-            'doc_idx': l.doc_idx,
-            'start_idx': 0,
-            'end_idx': len(l), # initial sort may be less than window size
-            'window_len': len(l)
-        }
-
-        order = np.array(model.score(**kwargs))
-        orig_idxs = np.arange(len(l))
-        l.doc_idx[orig_idxs], l.doc_texts[orig_idxs],  = l.doc_idx[order], l.doc_texts[order]
-        logger.info(f"Initial sort complete for query {qid}, len: {len(l)}")
-        if len(l) < model.window_size: 
-            if model.verbose: logger.info('Breaking out')
-            return l, r, True # breakout as only single sort is required
-        p = l[model.cutoff]
-        c = l[:model.cutoff]
-        b = l[model.cutoff+1:]
-        sub_window_size = model.window_size - 1 # account for addition of p
-
-        while len(c) < model.buffer and len(r) > 0:
-            l, r = split(r, sub_window_size)
-            l = p + l
-
-            kwargs = {
-                'qid': qid,
-                'query': query,
-                'doc_text': l.doc_texts.tolist(),
-                'doc_idx': l.doc_idx.tolist(),
-                'start_idx': 0,
-                'end_idx': len(l),
-                'window_len': len(l)
-            }
-
-            order = np.array(model.score(**kwargs))
-            orig_idxs = np.arange(len(l))
-            l[orig_idxs] = l[order]
-
-            p_idx = np.where(l.doc_idx == p.doc_idx[0])[0][0] # find index of pivot id
-            # add left of pivot to candidates and right of pivot to backfill
-            c = c + l[:p_idx]
-            b = b + l[p_idx+1:]
-        
-        # we have found no candidates better than p
-        if len(c) == model.cutoff - 1: 
-            top = c + p 
-            bottom = b + r
-            return top, bottom, True 
-        # we have found candidates better than p
-
-        # split c by budget b
-        c, ac = split(c, model.buffer)
-        ac = ac + p + b + r
-
-        return c, ac, False
-    
-def pivot(model, query : str, query_results : pd.DataFrame):
-    qid = query_results['qid'].iloc[0]
-    query_results = query_results.sort_values('score', ascending=False)
-    doc_idx = query_results['docno'].to_numpy()
-    doc_texts = query_results['text'].to_numpy()
-
-    indicator = False
-    num_iters = 0
-    c = RankedList(doc_texts, doc_idx)
-    b = RankedList()
-
-    while not indicator and num_iters < model.max_iters:
-        num_iters += 1
-        c, _b, indicator = _inner_pivot(model, qid, query, c)
-        b = b + _b
-    if num_iters == model.max_iters:
-        logger.warn(f"WARNING: max_iters reached for query {qid}")
-
-    out = c + b
-    return out.doc_idx, out.doc_texts
 
 def sliding_window(model, query : str, query_results : pd.DataFrame):
         qid = query_results['qid'].iloc[0]
